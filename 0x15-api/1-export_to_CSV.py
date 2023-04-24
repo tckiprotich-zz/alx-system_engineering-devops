79e028c224a0feaf0f6d0b2d350a5598874f10ef
COLLINS  TONUI <tckiprotich@gmail.com>
2023-04-24 22:51:38 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""Exports to-do list information to CSV format.
+all data from https://jsonplaceholder.typicode.com
+"""
 import csv
 import requests
 import sys
