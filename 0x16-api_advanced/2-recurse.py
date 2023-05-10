4103bc8c95c1d6b575710f09792f424e9be33d9b
COLLINS  TONUI <tckiprotich@gmail.com>
2023-05-10 09:19:53 +0300

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -17,35 +17,31 @@ Note:
 
 """
 import requests
-import sys
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    """Function to recursively get all hot articles in a subreddit
-    the subreeddit is passed during runtime"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    params = {'limit': 100, 'after': after}
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        data = response.json().get('data')
-        children = data.get('children')
-        if not children:
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    Queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given subreddit.
+
+    - If not a valid subreddit, return None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            hot_list.append(title)
+        after = req.json().get("data").get("after")
+
+        if after is None:
             return hot_list
-        hot_list.extend([child.get('data').get('title') for child in children])
-        after = data.get('after')
-        if not after:
-            return hot_list
-        return recurse(subreddit, hot_list, after=after)
+        else:
+            return recurse(subreddit, hot_list, after)
     else:
         return None
-
-
-if __name__ == "__main__":
-    subreddit = sys.argv[1]
-    hot_list = recurse(subreddit)
-    if hot_list:
-        for i, title in enumerate(hot_list, start=1):
-            print(f"{i}. {title}")
-    else:
-        print("No results found.")
