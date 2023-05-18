5347338b617216c4e58be5726fc42c4cd7223272
COLLINS  TONUI <tckiprotich@gmail.com>
2023-05-18 04:00:51 +0000

    horberton user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,20 +2,39 @@
 # to login with the holberton user and open
 # a file without any error message.
 
-# Create a file resource to ensure /etc/security/limits.d/holberton.conf exists
-file { '/etc/security/limits.d/holberton.conf':
-  ensure => 'file',
+# Create the holberton user
+
+user { 'holberton':
+  ensure => present,
 }
 
-# Add the necessary limits to the holberton.conf file
-file_line { 'increase-file-limits-for-holberton':
-  path => '/etc/security/limits.d/holberton.conf',
-  line => "holberton hard nofile 50000\nholberton soft nofile 50000",
+# Allow login for the holberton user
+file { '/etc/ssh/sshd_config':
+  ensure  => present,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => file('/etc/ssh/sshd_config').content + "
+Match User holberton
+    PasswordAuthentication yes
+",
+  notify  => Service['sshd'],
 }
 
-# Ensure the limits.conf file includes holberton.conf
-file_line { 'include-holberton-limits':
-  path => '/etc/security/limits.conf',
-  line => "## Include holberton limits\n@holberton -include /etc/security/limits.d/holberton.conf",
-  match => '^## End of file$',
+# Open a file without error messages
+file { '/path/to/your/file':
+  ensure  => present,
+  owner   => 'holberton',
+  group   => 'holberton',
+  mode    => '0644',
+  content => 'Content of your file',
+}
+
+# Restart the SSH service
+service { 'sshd':
+  ensure    => running,
+  enable    => true,
+  hasstatus => true,
+  restart   => '/usr/sbin/service sshd reload',
+  require   => File['/etc/ssh/sshd_config'],
 }
