b7c7f1fd692ceb6f975d37600aec412d47e382ae
COLLINS  TONUI <tckiprotich@gmail.com>
2023-05-18 03:48:05 +0000

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# fixing oue web server
+#  so that we get no errors
+exec { 'file limit':
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  provider => shell,
+}
