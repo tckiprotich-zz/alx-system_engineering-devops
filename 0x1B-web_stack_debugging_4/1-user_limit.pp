bcbc158dddd4eccd783c2a9a066a5791f0c194a5
COLLINS  TONUI <tckiprotich@gmail.com>
2023-05-18 04:02:12 +0000

    horberton user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,39 +2,13 @@
 # to login with the holberton user and open
 # a file without any error message.
 
-# Create the holberton user
-
-user { 'holberton':
-  ensure => present,
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
 }
 
-# Allow login for the holberton user
-file { '/etc/ssh/sshd_config':
-  ensure  => present,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => file('/etc/ssh/sshd_config').content + "
-Match User holberton
-    PasswordAuthentication yes
-",
-  notify  => Service['sshd'],
-}
-
-# Open a file without error messages
-file { '/path/to/your/file':
-  ensure  => present,
-  owner   => 'holberton',
-  group   => 'holberton',
-  mode    => '0644',
-  content => 'Content of your file',
-}
-
-# Restart the SSH service
-service { 'sshd':
-  ensure    => running,
-  enable    => true,
-  hasstatus => true,
-  restart   => '/usr/sbin/service sshd reload',
-  require   => File['/etc/ssh/sshd_config'],
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
